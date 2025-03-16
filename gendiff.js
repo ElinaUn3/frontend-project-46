94ed432c39371e68d693e7df9f8b78b6f2e71fa5
ElinaUn3 <uncholeg@yandex.ru>
2025-03-16 18:55:57 +0300

    first step

--- gendiff.js
+++ gendiff.js
@@ -9,9 +9,12 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0') 
-  .arguments('<filepath1> <filepath2>')
+  .arguments('<filepath1> <filepath2>') 
+  .option('-f, --format <type>', 'output format', 'stylish') 
   .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2); 
-    console.log(diff);
+    const options = program.opts();
+    const format = options.format;
+    const diff = genDiff(filepath1, filepath2, format); 
+    console.log(diff); 
   })
   .parse(process.argv);
